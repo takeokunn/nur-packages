0dd9e89febae2c086f2a03c70a443638f63bf589
takeokunn <bararararatty@gmail.com>
2024-11-05 17:26:32 +0900

    Add isucurd

--- flake.nix
+++ flake.nix
@@ -5,11 +5,7 @@
     let
       systems = [
         "x86_64-linux"
-        "i686-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "armv6l-linux"
-        "armv7l-linux"
+        "aarch64-darwin"
       ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
