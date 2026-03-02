bca313dc493f636eaf1c470fc507364224ff9cca
takeo obara <bararararatty+github-actions-gpg@gmail.com>
2026-03-02 04:33:05 +0000

    chore: automated package updates

--- pkgs/vimdoc-ja/default.nix
+++ pkgs/vimdoc-ja/default.nix
@@ -6,13 +6,13 @@
 
 vimUtils.buildVimPlugin {
   pname = "vimdoc-ja";
-  version = "unstable-2026-02-27";
+  version = "unstable-2026-03-01";
 
   src = fetchFromGitHub {
     owner = "vim-jp";
     repo = "vimdoc-ja";
-    rev = "b7731d9d8add3f3dd948b26d7f47d4ee266eba39";
-    hash = "sha256-D46vuox4aZTj2KvycI4nr+tomrvCdHJclcTMRAhUmvQ=";
+    rev = "26e204ce3b0b03d5e2f69f34dd022cfca19945ac";
+    hash = "sha256-Hxtw1kgW8VQhoqrMnPKl74W6R7WM+rJjA55EoCayY38=";
   };
 
   meta = {
