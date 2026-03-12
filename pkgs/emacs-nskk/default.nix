1530e77cac1b5b7ebaffcd8453a2d35817dd1871
takeokunn <bararararatty@gmail.com>
2026-03-13 06:28:36 +0900

    update

--- pkgs/emacs-nskk/default.nix
+++ pkgs/emacs-nskk/default.nix
@@ -6,13 +6,13 @@
 
 emacsPackages.trivialBuild {
   pname = "nskk";
-  version = "0.1.3";
+  version = "0.1.4";
 
   src = fetchFromGitHub {
     owner = "takeokunn";
     repo = "nskk.el";
-    rev = "v0.1.3";
-    hash = "sha256-Z507Ko2LnZ2I2UEJwUJgfxSwqLr2hT64qCgIuWKnVCA=";
+    rev = "v0.1.4";
+    hash = "sha256-+146qrDUpjDy8r8RyMgOScnVR4KcWn/6E98bg5xLf/M=";
   };
 
   meta = {
