83d56342828069bcb8c7fa3b78c3f444b71fab16
takeokunn <bararararatty@gmail.com>
2026-03-12 16:39:14 +0900

    chore(emacs-nskk): bump nskk to v0.1.3 and update source hash

--- pkgs/emacs-nskk/default.nix
+++ pkgs/emacs-nskk/default.nix
@@ -6,13 +6,13 @@
 
 emacsPackages.trivialBuild {
   pname = "nskk";
-  version = "0.1.2";
+  version = "0.1.3";
 
   src = fetchFromGitHub {
     owner = "takeokunn";
     repo = "nskk.el";
-    rev = "v0.1.2";
-    hash = "sha256-bFhPl3TAP6XglHhQBeVGuybgqvj7f7m41CNkHQ7dEvQ=";
+    rev = "v0.1.3";
+    hash = "sha256-Z507Ko2LnZ2I2UEJwUJgfxSwqLr2hT64qCgIuWKnVCA=";
   };
 
   meta = {
