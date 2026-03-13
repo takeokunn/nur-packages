a41815fdae64d4d61b191277fc0808a496662bd8
takeo obara <bararararatty+github-actions-gpg@gmail.com>
2026-03-13 09:25:33 +0000

    chore: automated package updates

--- pkgs/emacs-nskk/default.nix
+++ pkgs/emacs-nskk/default.nix
@@ -6,13 +6,13 @@
 
 emacsPackages.trivialBuild {
   pname = "nskk";
-  version = "0.1.7";
+  version = "0.1.8";
 
   src = fetchFromGitHub {
     owner = "takeokunn";
     repo = "nskk.el";
-    rev = "v0.1.7";
-    hash = "sha256-m0QbvdhuBO9EtL/yNCEf2eNsfHHU66koOoy7rovVHjo=";
+    rev = "v0.1.8";
+    hash = "sha256-6VBnZUQbAaMXrBcgCZIkEEPMDTkw/9GZyMYFIzLTja8=";
   };
 
   meta = {
