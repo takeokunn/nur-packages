a40cc7d232adf2359cc5205439fcc748f2081887
takeo obara <bararararatty+github-actions-gpg@gmail.com>
2026-05-05 06:24:42 +0000

    chore: automated package updates

--- pkgs/gogcli/default.nix
+++ pkgs/gogcli/default.nix
@@ -5,14 +5,14 @@
 }:
 buildGo126Module rec {
   pname = "gogcli";
-  version = "0.14.0";
+  version = "0.15.0";
   src = fetchFromGitHub {
     owner = "steipete";
     repo = "gogcli";
     rev = "refs/tags/v${version}";
-    hash = "sha256-aau1w6b4nBdTMUTeX0LwV+8YPP5YeghE0iWSaHQXBFQ=";
+    hash = "sha256-8ffexCQWQXaaEyiWIKxg8M2VdgLDMqTiadCu/8v3DE0=";
   };
-  vendorHash = "sha256-UTkuqDXo6TnmZBuk18yhqBTT0+u/CebR4/uZw8XOX2k=";
+  vendorHash = "sha256-gcbzmq3TvG9Ii7J2B48nbwNkUUvcdfTqU3RlcxGGqyE=";
 
   doCheck = false;
 
