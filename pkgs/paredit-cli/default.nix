1bb32c62f7795fb337b7644d3bfa247e4c2615cd
takeo obara <bararararatty+github-actions-gpg@gmail.com>
2026-07-30 06:58:17 +0000

    chore: automated package updates

--- pkgs/paredit-cli/default.nix
+++ pkgs/paredit-cli/default.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "paredit-cli";
-  version = "1.2.1";
+  version = "1.3.0";
 
   src = fetchFromGitHub {
     owner = "takeokunn";
     repo = "paredit-cli";
     rev = "v${version}";
-    hash = "sha256-e0tQ050RLNJYl6Kpdp1mqJi5RdYevZlpr757zp6tddw=";
+    hash = "sha256-UD8Nws8wpybWfqSUfuGykQHr2PldEU3ALFbHMVPQn8U=";
   };
 
-  cargoHash = "sha256-n+JwjeAhq4YZt8eTcIpEw9xe59js495MqapySuGAV4M=";
+  cargoHash = "sha256-jspPyAEViiwnGsbQug170DPYFJM+la3pHRsiTYZBEL0=";
 
   meta = {
     description = "Structure-editing CLI for safe S-expression refactoring by AI coding agents";
